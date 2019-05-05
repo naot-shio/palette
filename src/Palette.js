7afb600cabc01b25afdaa811738a68ba901879d7
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-05-05 16:15:03 +0900

    Add palette

---
+++ src/Palette.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar */}
+        <div className="Palette-colors">
+          <h1>Palette</h1>
+          {/* Components */}
+        </div>
+        {/* Footer */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
