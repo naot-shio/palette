0f0b3589ec92a1f28dc90538533c2ee079a4b2ad
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-05-10 14:23:38 +0900

    Add delete icon to mini palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import styles from './styles/MiniPaletteStyles'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 function MiniPalette(props) {
   const { classes, paletteName, emoji, colors } = props;
@@ -10,9 +11,13 @@ function MiniPalette(props) {
       style={{backgroundColor: color.color}}
       key={color.name}
     />
-  ))
+  ));
+
   return (
     <div className={classes.root} onClick={props.handleClick}>
+      <div class={classes.delete}>
+        <DeleteIcon className={classes.deleteIcon} style={{transition: "all 0.3s ease-in-out"}}/>
+      </div>
       <div className={classes.colors}>
         {miniColorBoxes}
       </div>
