9e1939315e80c085b9032c63dfe45859bf0684e6
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-05-10 16:31:09 +0900

    Add bg svg to PaletteList

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,12 +1,16 @@
-import sizes from './sizes'
+import sizes from './sizes';
+import bg from './bg.svg';
 
 export default {
   root: {
-    backgroundColor: "blue",
     height: "100vh",
     display: "flex",
     alginItems: "flex-start",
-    justifyContent: "center"
+    justifyContent: "center",
+    /* background by SVGBackgrounds.com */
+    backgroundColor: "#270faa",
+    backgroundImage: `url(${bg})`,
+    overflow: "scroll"
   },
   container: {
     width: "50%",
@@ -26,7 +30,7 @@ export default {
     color: "white",
     "& a": {
       color: "white",
-      marginTop: "1.7em"
+      marginTop: "2.5rem"
     }
   },
   palette: {
@@ -35,11 +39,8 @@ export default {
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
     gridGap: "1.5rem",
-    [sizes.down("md")]: {
-      gridTemplateColumns: "repeat(2, 50%)"
-    },
-    [sizes.down("xs")]: {
-      gridTemplateColumns: "repeat(1, 100%)",
-    },
+  },
+  heading: {
+    fontSize: "2rem"
   }
 }
